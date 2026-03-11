f258b1313420c1989fd35195787fc1ddbe515844
Administrator <morfiklol777@gmail.com>
2026-03-11 14:35:56 +0700

    Добавил тесты без логина с заполненным паролем и без пароля с заполненным логином

--- src/main/java/core/base/BasePage.java
+++ src/main/java/core/base/BasePage.java
@@ -1,31 +1,46 @@
 package core.base;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
-public abstract class BasePage {
+public class BasePage {
 
     //Примеры общих элементов, которые могут использоваться на разных страницах
     protected SelenideElement headerLogo = $("[tsid='toolbar_logo']");
     protected SelenideElement searchField = $("[name='st.query']");
     protected SelenideElement vkServices = $("[data-l+'t,vk_ecosystem']");
+    protected SelenideElement acceptCookieButton = $(".cb_accept");
+    protected SelenideElement acceptPrivacyButton = $(".cmptxt_btn_yes");
 
     //Метод для поиска по сайту
+    @Step("Выполняем поиск по сайту с запросом: {query}")
     public void search(String query) {
         searchField.shouldBe(visible).setValue(query).pressEnter();
     }
 
     //Пример общего метода для клика по иконке уведомлений
+    @Step("Открываем VK Services")
     public void openVkServices() {
         vkServices.shouldBe(visible).click();
     }
 
     //Клик на логотип ОК
+    @Step("Кликаем на логотип ОК")
     public void clickLogo() {
         headerLogo.shouldBe(visible).click();
     }
 
+    @Step("Принимаем куки")
+    public void acceptCookie() {
+        acceptCookieButton.shouldBe(visible).click();
+    }
+
+    @Step("Принимаем политику конфиденциальности")
+    public void acceptPrivacyButton() {
+        acceptPrivacyButton.shouldBe(visible).click();
+    }
     //Другие общие методы, например, для проверки загрузки страницы, авторизации и т.д.
 }
